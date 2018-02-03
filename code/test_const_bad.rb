3623917d39565ecc12880df4fa9f6ddab2179a5a
Pramod Jacob <psjacob68@gmail.com>
2018-02-03 15:34:52 -0500

    improve bad test const code snippet

--- code/test_const_bad.rb
+++ code/test_const_bad.rb
@@ -1,7 +1,15 @@
 describe 'test object' do
-  BAR = 'some_value'
+  BAR = 'some value'
 
   it 'is equal to BAR' do
     assert_equal test_obj, BAR
+  end
+end
+
+describe 'another test object' do
+  BAR = 'another value'
+
+  it 'is equal to BAR' do
+    assert_equal another_test_obj, BAR
   end
 end
