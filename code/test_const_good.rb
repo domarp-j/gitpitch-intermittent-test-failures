a21f62901a814538f715a1b28a5c6928bdc5fc50
Pramod Jacob <psjacob68@gmail.com>
2018-02-04 12:33:51 -0500

    make test const good/bad code snippets more symmetrical

--- code/test_const_good.rb
+++ code/test_const_good.rb
@@ -1,7 +1,15 @@
 describe 'test object' do
-  let(:bar) { 'some_value' }
+  let(:bar) { 'some value' }
 
   it 'is equal to bar' do
     assert_equal test_obj, bar
+  end
+end
+
+describe 'another test object' do
+  let(:bar) { 'another value' }
+
+  it 'is equal to bar' do
+    assert_equal another_test_obj, bar
   end
 end
