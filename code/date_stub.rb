5584b3457ff550fd18f08d546814901184cd2d3d
Pramod Jacob <psjacob68@gmail.com>
2018-02-06 20:34:59 -0500

    reduce size of date stub code

--- code/date_stub.rb
+++ code/date_stub.rb
@@ -1,10 +1,10 @@
 describe '#calculate_gaps' do
   let(:date_ranges) do
     [
-      [Date.parse('June 1 2017'), Date.parse('December 30 2017')],
-      [Date.parse('September 3 2016'), Date.parse('April 1 2017')],
-      [Date.parse('March 3 2014'), Date.parse('September 2 2016')],
-      [Date.parse('February 15 2013'), Date.parse('January 10 2014')]
+      ['June 1 2017', 'December 30 2017'],
+      ['September 3 2016', 'April 1 2017'],
+      ['March 3 2014', 'September 2 2016'],
+      ['February 15 2013', 'January 10 2014']
     ]
   end
 
@@ -12,8 +12,8 @@ describe '#calculate_gaps' do
     Date.stubs(:today).returns(Date.parse('December 31 2017'))
   end
 
-  it 'calculates a date gap between five years ago and the current day' do
+  it 'calculates date gap from five years' do
     result = calculate_gaps(date_ranges)
-    assert gap_present(Date.parse('December 31 2012'), Date.parse('February 14 2013'))
+    assert gap_present('December 31 2012', 'February 14 2013')
   end
 end
